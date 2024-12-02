7253efdc64c6bc6a1d3d70a96c250df823d78f79
hnnsb <hannes.b@wtnet.de>
2024-12-02 23:23:13 +0100

    formatting

--- 01.py
+++ 01.py
@@ -1,12 +1,13 @@
-import sys 
+
+import sys
 
 with open(sys.argv[1] if len(sys.argv) > 1 else sys.argv[0][:2] + ".in") as file:
     data = file.readlines()
-    
+
 data = [line.split() for line in data]
 data = [list(map(int, line)) for line in data]
 data = list(zip(*data))
-a,b = [list(s) for s in data]
+a, b = [list(s) for s in data]
 
 a, b = sorted(a), sorted(b)
 part1 = 0
@@ -14,8 +15,8 @@ for i in range(len(a)):
     d = abs(a[i]-b[i])
     part1 += d
 print(part1)
-    
-part2 = 0 
+
+part2 = 0
 for e in a:
     part2 += e * b.count(e)
 print(part2)
--- 02.py
+++ 02.py
@@ -1,28 +1,32 @@
-import sys 
+import sys
 
 with open(sys.argv[1] if len(sys.argv) > 1 else sys.argv[0][:2] + ".in") as file:
     data = file.readlines()
-    
+
 data = [line.split() for line in data]
 data = [list(map(int, line)) for line in data]
-    
+
+
 def isSafe(line):
     descending = all(line[i] > line[i+1] for i in range(len(line)-1))
     ascending = all(line[i] < line[i+1] for i in range(len(line)-1))
-    good_diffs = all(1 <= abs(line[i] - line[i+1]) <= 3 for i in range(len(line)-1))
+    good_diffs = all(1 <= abs(line[i] - line[i+1])
+                     <= 3 for i in range(len(line)-1))
     return (descending or ascending) and good_diffs
-    
+
+
 part1 = 0
 part2 = 0
-for line in data:    
+for line in data:
     if isSafe(line):
         part1 += 1
-    
-    for i in range(len(line)+1): # Check leaving each one out, +1 to check whole list.
+
+    # Check leaving each one out, +1 to check whole list.
+    for i in range(len(line)+1):
         new_line = line[:i] + line[i+1:]
         if isSafe(new_line):
-            part2 +=1
-            break # Save time if we find one that is safe
-        
+            part2 += 1
+            break  # Skip other checks if one option is sage.
+
 print(f"Part 1: {part1}")
 print(f"Part 2: {part2}")
