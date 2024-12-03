884c100683c8c9d8bdd97682379a96b4ce518bb8
Hannes <77492703+hnnsb@users.noreply.github.com>
2024-12-03 08:35:49 +0100

    day 3

---
+++ 03.py
@@ -0,0 +1,27 @@
+import sys
+import re
+
+with open(sys.argv[1] if len(sys.argv) > 1 else sys.argv[0][-5:-3] + ".in") as file:
+    data = file.read()
+
+part1 = 0
+for match in re.findall(r"mul\(\d+,\d+\)", data):
+    numbers = match[4:-1]
+    a,b = map(int, numbers.split(","))
+    part1 += a * b
+
+print(part1)
+
+part2 = 0
+do = True
+for match in re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", data):
+    if match.startswith("don't()"):
+        do = False
+    elif match.startswith("do()"):
+        do = True
+    elif do:
+        numbers = match[4:-1]
+        a,b = map(int, numbers.split(","))
+        part2 += a * b
+
+print(part2)
