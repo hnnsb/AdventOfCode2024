fac083bbebd25e38bf13a9a36f47219e6e789034
hnnsb <hannes.b@wtnet.de>
2024-12-10 06:43:04 +0100

    update init script to not remove trailing zeros

--- initday.bat
+++ initday.bat
@@ -5,7 +5,7 @@ IF ["%1"]==[""] GOTO ERROR
 COPY "template.py" "%1.py"
 set num=%1
 REM Remove leading zeros from the num variable
-set num=%num:0=%
+if "%num:~0,1%"=="0" set num=%num:~1%
 
 START /B code . "%1.in" "%1.py"
 START /B "" https://adventofcode.com/2024/day/%num%
