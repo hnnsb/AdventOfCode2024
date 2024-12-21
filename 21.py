d938f7d3a951801b61e3c1bbc138309e43e87caa
hnnsb <hannes.b@wtnet.de>
2024-12-21 15:44:49 +0100

    day 21 cleanup

--- 21.py
+++ 21.py
@@ -1,19 +1,11 @@
 from functools import cache
-from itertools import permutations
 import re
 import sys
-from helper.grid import *
 
-with open(sys.argv[1] if len(sys.argv) > 1 else sys.argv[0][-5:-3] + ".in") as file:
+with open("input/" + (sys.argv[1] if len(sys.argv) > 1 else sys.argv[0][-5:-3]) + ".in") as file:
     data = file.readlines()
 data = [row.strip() for row in data]
 
-MOVES = {
-    '^': (-1, 0),
-    'v': (1, 0),
-    '<': (0, -1),
-    '>': (0, 1)
-}
 number_pad = [['7', '8', '9'],
               ['4', '5', '6'],
               ['1', '2', '3'],
@@ -45,24 +37,25 @@ def get_step(start, end, isNumberPad):
     assert False
 
 
+@cache
 def min_length_for(code, limit=2, depth=0):
     if depth > limit:
         return len(code)
 
     start = "A"
-    sequence = []
+    sequences = []
     for button in code:
-        seq = get_step(start, button, depth == 0)
-        sequence.append(seq)
-
+        step = get_step(start, button, depth == 0)
+        sequences.append(step)
         start = button
 
-    return min_length_for("".join(sequence), limit, depth+1)
+    # Do it per button, smaller sequence --> better caching.
+    # All deeper sequnce parts start and end on "A", therefore this is possible.
+    return sum(min_length_for(button_sequence, limit, depth+1) for button_sequence in sequences)
 
 
 def solve(code, limit=2):
     res = min_length_for(code, limit)
-    print(res, " * ", re.findall(r"\d+", code)[0])
     res = res*int(re.findall(r"\d+", code)[0])
     return res
 
@@ -71,8 +64,6 @@ part1 = 0
 part2 = 0
 for code in data:
     part1 += solve(code)
-print(part1)  # too high 190424
-exit()
-for code in data:
     part2 += solve(code, 25)
+print(part1)
 print(part2)
